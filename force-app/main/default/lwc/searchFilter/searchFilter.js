0b538d3b2a5a78c064f8292cf2098adedc0e2a21
Кирил Кириченко <125361474+kiri4ook@users.noreply.github.com>
2023-02-24 17:13:48 +0200

    Add files via upload

--- force-app/main/default/lwc/searchFilter/searchFilter.js
+++ force-app/main/default/lwc/searchFilter/searchFilter.js
@@ -1,47 +1,66 @@
-import { LightningElement, api, wire, track } from 'lwc';
-import getContactList from '@salesforce/apex/ContactController.getContactList';
-import getContact from '@salesforce/apex/ContactController.getContact';
-const COLUMNS = [
-   { label: 'First Name', fieldName:'FirstName', type: 'text', sortable: true},
-   { label: 'Last Name', fieldName: 'LastName', type: 'text'},
-   { label: 'Email', fieldName: 'Email', type: 'email'},
-   { label: "Account Name", fieldName: "recordLink", type: "url",
-      typeAttributes: { label: { fieldName: 'Account_Name' }}},
-   { label: 'Mobile Phone', fieldName: 'Phone', type: 'phone', sortable: true },
-   { label: 'Created Date', fieldName:  'CreatedDate', type: 'date', sortable: true, typeAttributes:{
-   year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit"}}
-];
-export default class SearchFilter extends LightningElement {
-   @track contacts;
-   columns = COLUMNS;
-   @track error;
-   @track contacts;
-   @wire(getContactList)
-   wiredAccounts({error, data}) {
-      if (data) {
-         var ObjData = JSON.parse(JSON.stringify(data));
-         ObjData.forEach(item =>{
-            item.recordLink = item.Account!=undefined ? '/lightning/r/Account/' +item.AccountId +'/view' : ''
-            
-      });
-         this.contacts = ObjData;
-     } else if (error) {
-         this.error = error;
-     }
-   }
-     searchKey;
-    handelSearchKey(event){
-        this.contacts = event.target.value;
-    }
-    SearchAccountHandler(){
-        getContact({textkey: this.contacts})
-        .then(result => {
-                this.contacts = result;
-        })
-        .catch( error=>{
-            this.contacts = null;
-        });
+import { LightningElement, api, wire, } from 'lwc';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
 
-    }
+export default class SearchFilter extends LightningElement {
+
+   columns = [
+      { label: 'First Name', fieldName:'FirstName', type: 'text'},
+      { label: 'Last Name', fieldName: 'LastName', type: 'text'},
+      { label: 'Email', fieldName: 'Email', type: 'email'},
+      { label: "Account Name", fieldName: "recordLink", type: "url",
+         typeAttributes: { label: { fieldName: 'AccountName' }}},
+      { label: 'Mobile Phone', fieldName: 'Phone', type: 'phone'},
+      { label: 'Created Date', fieldName:  'CreatedDate', type: 'date', typeAttributes:{
+         day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit"}}
+   ];
+   error;
+   contacts;
+   searchKey;
+   initialRecords;
+   @wire (getContacts)
+
+   wiredContacts ({error, data}) {
+      if (data) {
+         let contactData = JSON.parse(JSON.stringify(data));
+         contactData.forEach(record =>{
+            if (record.AccountId) {
+               record.recordLink = "/" + record.AccountId;
+               record.AccountName = record.Account.Name; 
+            }
+         });
+         this.contacts = contactData;
+         this.initialRecords = contactData;
+      } else if (error) {
+         this.error = error;
+      }
+   }
+
+   handelSearchKey (event) {
+      this.searchKey = event.target.value.toLowerCase();  
+   }
+    
+   searchContactHandler(){
+      if (this.searchKey) {
+         this.contacts = this.initialRecords;
+         if (this.contacts) {
+            let recs = [];             
+            for (let rec of this.contacts) {
+               let valuesArray = Object.values(rec);
+               for (let val of valuesArray) {
+                  let strVal = String(val);                     
+                  if (strVal) {
+                     if (strVal.toLowerCase().includes(this.searchKey)) {
+                        recs.push(rec);
+                        break;                     
+                     }
+                  }
+               }                 
+            }
+            this.contacts = recs;
+         }
+      } else {
+         this.contacts = this.initialRecords;
+      }              
+   }
 }
 
